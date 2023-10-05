f4b48395093ec5e26b1eeb00678c0029d76b05a2
Toalra <K0$k@$1mk@2950>
2023-10-05 20:19:33 +0300

    1

---
+++ src/test/java/quru/qa/in/reqres/models/LoginResponseModel.java
@@ -0,0 +1,14 @@
+package quru.qa.in.reqres.models;
+
+public class LoginResponseModel {
+
+    String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
--- src/test/java/quru/qa/tests/StatusTests.java
+++ src/test/java/quru/qa/tests/StatusTests.java
@@ -2,22 +2,25 @@ package quru.qa.tests;
 
 import org.junit.jupiter.api.Test;
 import quru.qa.TestBase;
+import quru.qa.in.reqres.models.LoginBodyModel;
+import quru.qa.in.reqres.models.LoginResponseModel;
 
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatusTests extends TestBase {
 
     @Test
     void successRegister() {
-        String regBody = "{\n" +
-                "    \"email\": \"eve.holt@reqres.in\",\n" +
-                "    \"password\": \"pistol\"\n" +
-                "}";
-        given()
+        LoginBodyModel regBody = new LoginBodyModel();
+        regBody.setEmail("eve.holt@reqres.in");
+        regBody.setPassword("pistol");
+
+        LoginResponseModel response = given()
                 .log().uri()
                 .log().body()
                 .contentType(JSON)
@@ -28,7 +31,9 @@ public class StatusTests extends TestBase {
                 .log().body()
                 .statusCode(200)
                 .body("id", is(4))
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+                .extract().as(LoginResponseModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
     }
 
     @Test
